d89c9626016dc97c634e4ed03b9a0ca42f5c2677
Ed <silva.email@yahoo.com>
2024-03-14 22:27:28 -0500

    Add URL and raw URL for Google

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const vm = Vue.createApp({
         return {
             firstName: "John",
             lastName: "Doe",
+            url:"https://www.google.com",
+            raw_url:'<a href="https://www.google.com" target="_blank">Google</a>',
         };
     },
     methods: {
