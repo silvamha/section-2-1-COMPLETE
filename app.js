457f295d152809780178552a9e900b601586bb18
Ed <silva.email@yahoo.com>
2024-03-15 05:12:03 -0500

    Add age property and increment method to Vue app

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const vm = Vue.createApp({
             lastName: "Doe",
             url:"https://www.google.com",
             raw_url:'<a href="https://www.google.com" target="_blank">Google</a>',
+            age:20,
         };
     },
     methods: {
         fullName() {
             return `${this.firstName} ${this.lastName.toUpperCase()}`;
         },
+
+        increment(){
+            let count
+            count = this.age++
+        }
     },
 }).mount("#app");
 
