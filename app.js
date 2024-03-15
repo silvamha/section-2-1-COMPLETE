5aaa6a2ca721363ff178f3c2530cd36a11a85014
Ed <silva.email@yahoo.com>
2024-03-15 08:04:53 -0500

    End of Section 2.1 complete code

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const vm = Vue.createApp({
-    data() {
-        return {
-            firstName: "John",
-            lastName: "Doe",
-            url:"https://www.google.com",
-            raw_url:'<a href="https://www.google.com" target="_blank">Google</a>',
-            age:20,
-        };
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe",
+      middleName: "",
+      url: "https://www.google.com",
+      raw_url: '<a href="https://www.google.com" target="_blank">Google</a>',
+      age: 20,
+    };
+  },
+  methods: {
+    increment() {
+      this.age++;
     },
-    methods: {
-        fullName() {
-            return `${this.firstName} ${this.lastName.toUpperCase()}`;
-        },
-
-        increment(){
-            let count
-            count = this.age++
-        }
+    updateLastName(msg, e) {
+      //   console.log(msg);
+      this.lastName = e.target.value;
     },
+    updateAge(e) {
+      this.age = e.target.value;
+    },
+    updateMiddleName(e) {
+      this.middleName = e.target.value;
+    },
+  },
+  computed: {
+    fullName() {
+      console.log("Full name cumputed property was called!");
+      return `${this.firstName} ${
+        this.middleName
+      } ${this.lastName.toUpperCase()}`;
+    },
+  },
+  watch:{
+    age(newVal, oldVal){
+        setTimeout(() => {
+            this.age = 20
+        },2000)
+    }
+  }
 }).mount("#app");
 
 // setTimeout(() => {
