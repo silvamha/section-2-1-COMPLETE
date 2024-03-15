2e5845e281de1f4ef61b67376e60c0146b3bc14c
Ed <silva.email@yahoo.com>
2024-03-14 19:32:28 -0500

    Refactor app.js and index.html

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-Vue.createApp({}).mount('#app')
+const vm = Vue.createApp({
+    data() {
+        return {
+            firstName: "John",
+            lastName: "Doe",
+        };
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}`;
+        },
+    },
+}).mount("#app");
+
+// setTimeout(() => {
+//     vm.firstName = 'Bob'
+// },2000)
+
+// Vue.createApp({
+//     data(){
+//         return {
+//             firstName:'Jane',
+//             lastName: "Doe"
+//         }
+//     }
+// }).mount('#app2')
